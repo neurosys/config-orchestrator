d9e4c5761cf8db53f66be1e94fb049e59b27f9cf
NeuroSys <you@example.com>
2020-02-25 00:00:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import sys
+import xml.etree.ElementTree as ET
+
+# ToDo
+# - Options: 
+#    * generate only one file
+#    * list IDs from config file
+#    * backup if exists
+#    * do not overwrite
+
+isDebug = False
+
+def dbg(msg):
+    global isDebug
+    if isDebug == True:
+        print(msg)
+
+def showHelp():
+    helpText = """
+Usage: orchestrator.py [OPTION]... 
+Assemble config files from pieces based on a xml file that describes the operation
+Example: orchestrator.py --config=sample.xml 
+
+Options:
+    --config=PATH          PATH to the xml describing the steps for creating the files
+    --debug                Enable debug information
+    --help                 Shows this help message
+    --only=SECTION         Generate only SECTION and ignore all other files
+    --list                 List the files for which we have assemble steps
+    --no-overwrite         Don't overwrite the destinations
+    --backup-if-exists     If the target exists, it will be backed up as <name>_bkp
+    """
+    print(helpText)
+
+class Params():
+    def __init__(self, args):
+        self.pathToConfigFile = None
+        self.sectionToGenerate = None
+        self.generateAll = True
+        self.showHelp = False
+        self.showDebug = False
+        self.listIds = False
+        self.noOverwrite = False
+        self.backupIfExists = False
+        self._parseParams(args)
+        self._validate()
+
+    def _parseParams(self, args):
+        global isDebug
+        # First parameter is the name of the script and we don't care aout it
+        for arg in args[1 : ]:
+            dbg("TRACE: Params::_parseParams() arg = " + arg)
+            if arg == "--debug":
+                self.showDebug = True
+                isDebug = True
+            elif arg == "--help":
+                self.showHelp = True
+            elif arg.startswith("--config=") == True:
+                self.pathToConfigFile = arg.split("=")[1]
+            elif arg.startswith("--only="):
+                self.sectionToGenerate = arg.split("=")[1]
+                self.generateAll = False
+            elif arg == "--list":
+                self.listIds = True
+            else:
+                print("ERROR: Unknown parameter '" + arg + "'")
+
+    def __str__(self):
+        textRepresentation = "Params {\n"
+        textRepresentation += "\tpathToConfigFile = " + str(self.pathToConfigFile) + "\n"
+        textRepresentation += "\tsectionToGenerate = " + str(self.sectionToGenerate) + "\n"
+        textRepresentation += "\tgenerateAll = " + str(self.generateAll) + "\n"
+        textRepresentation += "\tshowHelp = " + str(self.showHelp) + "\n"
+        textRepresentation += "\tshowDebug = " + str(self.showDebug) + "\n"
+        textRepresentation += "\tlistIds = " + str(self.listIds) + "\n"
+        textRepresentation += "\tnoOverwrite = " + str(self.noOverwrite) + "\n"
+        textRepresentation += "\tbackupIfExists = " + str(self.backupIfExists) + "\n"
+        textRepresentation += "}\n"
+        return textRepresentation
+
+    def _validate(self):
+        dbg(str(self))
+        if self.showHelp or self.listIds:
+            return True
+        elif self.pathToConfigFile == None:
+            print("ERROR: No config file specified")
+            self.showHelp = True
+
+class XmlParser():
+    def __init__(self, pathToXml):
+        self.pathToXml = pathToXml
+        self.tree = ET.parse(self.pathToXml)
+        self.root = self.tree.getroot()
+
+    def parse(self):
+        self.printNode(self.root)
+        for child in self.root:
+            self.printNode(child)
+
+    def printNode(self, node):
+        print("<" + node.tag + ">")
+        for k in node.attrib:
+            print("\t" + k + "=" + node.attrib[k])
+
+class FileBuilder():
+    def __init__(self, pathToConfig):
+        self._confFile = pathToConfig
+
+
+
+if __name__ == "__main__":
+    config = Params(sys.argv)
+
+    if config.showHelp == True:
+        showHelp()
+        sys.exit(0)
+
+    parser = XmlParser(config.pathToConfigFile)
+    parser.parse()
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 main.py --debug --config=sample.xml --only=i3config --debug 
